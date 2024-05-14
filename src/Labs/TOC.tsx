6c29e63bee02079fec1467d3f1f16d5af511a0f9
stella219 <zhang.stella@northeastern.edu>
2024-05-13 19:18:39 -0700

    Add student name

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -1,6 +1,7 @@
 export default function TOC() {
     return (
       <ul>
+        <h2>Name: Yanan Zhang (Stella)</h2>
         <li><a id="wd-a"  href="#/Labs">Labs</a></li>
         <li><a id="wd-a1" href="#/Labs/Lab1">Lab 1</a></li>
         <li><a id="wd-a2" href="#/Labs/Lab2">Lab 2</a></li>
